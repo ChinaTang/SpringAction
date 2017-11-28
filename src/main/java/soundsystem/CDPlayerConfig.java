a4449432913f9c346ab40e70d69e009d14ff6154
tangdi <tangdi0802@foxmail.com>
2017-11-28 17:49:12 +0800

    配置练习

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -5,19 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = SgtPeppers.class)
 public class CDPlayerConfig {
-    @Bean
-    public CompactDisc sgtPeppers(){
-        return new SgtPeppers();
-    }
 
-    @Bean
-    public MediaPlayer cdPlayer(CompactDisc compactDisc){
-        return new CDPlayer(compactDisc);
-    }
-
-    @Bean(name = "anthoCD")
-    public MediaPlayer cdPlayer2(CompactDisc compactDisc){
-        return new CDPlayer(compactDisc);
-    }
 }
---
+++ src/main/java/soundsystem/SoundSystemConfig.java
@@ -0,0 +1,11 @@
+package soundsystem;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@Import(CDPlayerConfig.class)
+@ImportResource("classpath:blankConfig.xml")
+public class SoundSystemConfig {
+}
